ae95d71b727c8eb344fec523cce2ce94b7864b37
Raphael Creer <rcreer@lab1r4s5.42abudhabi.ae>
2024-10-21 19:58:47 +0400

    second commit

---
+++ src/minishell.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+char	**ft_split(char *s, char c);
--- src/test.c
+++ src/test.c
@@ -1,9 +1,29 @@
+#include "minishell.h"
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+// char **tokenization(char *input)
+// {
+//     char **res;
+
+//     res = ft_split("")
+// }
 int main(int ac, char **av)
-{
+{   
     while(1)
     {
-        
+        char *input;
+        // char **tokens;
+
+        input = readline("minishell> ");
+        if(input) 
+        {   
+            if(!ft_strncmp(input,"exit",5))
+                exit(EXIT_SUCCESS);
+            // tokens = tokenization(input);
+            free(input);
+        }
     }
+    return 0;
 }
