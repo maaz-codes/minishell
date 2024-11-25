4da8a0f9a829d4e01ee2c17dcb795b820723b0bb
Raphael <rcreer@lab1r5s4.42abudhabi.ae>
2024-11-25 20:34:19 +0400

    exit cmd updated

--- builtins/exit_cmd.c
+++ builtins/exit_cmd.c
@@ -51,20 +51,18 @@ unsigned long long symbol_check(char *str, int *check)
     return (exit_num);
 }
 
-void num_valid_check(unsigned long long exit_num, int check, unsigned long long lm, char *str)
+void num_valid_check(unsigned long long e, int c, unsigned long long lm, char *str)
 {
-    if(exit_num > LONG_MAX && check == 1)
+    if(e > LONG_MAX && c == 1)
         error_msg(str);
-    else if(exit_num > lm && check == 0)
+    else if(e > lm && c == 0)
         error_msg(str);
 }
 void exit_cmd(t_path **paths, char **str)
 {   
     unsigned long long exit_num;
-    unsigned long long lm_check;
     int check;
 
-    lm_check = (ULONG_MAX - LONG_MIN) + 1;
     check = 1;
     if(!ft_strncmp(str[0],"exit",5) && str[1] != NULL && str[2] != NULL)
     {
@@ -76,7 +74,7 @@ void exit_cmd(t_path **paths, char **str)
         if(!valid_num(str[1]) || valid_num(str[1]) >= 20)
             error_msg(str[1]);
         exit_num = symbol_check(str[1],&check);
-        num_valid_check(exit_num,check,lm_check,str[1]);
+        num_valid_check(exit_num,check,(ULONG_MAX - LONG_MIN) + 1,str[1]);
         if(!check)
             neg_num_exit(paths,exit_num);
         if(exit_num >= 256 && check == 1)
