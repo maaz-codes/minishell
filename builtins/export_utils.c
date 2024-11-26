ccb7dc4a698acab52c31e95fb2c44936a248f167
RaphCodes1 <raphaelcreer@gmail.com>
2024-11-27 02:37:56 +0400

    added invalid identifier error msg when the export argument is invalid, does not exit but stops the process

--- builtins/export_utils.c
+++ builtins/export_utils.c
@@ -12,6 +12,51 @@ char *holder_env(char *str, int len, int check)
     return (holder);
 }
 
+int error_exp(char *str, char **res, char **sep)
+{
+    printf("export: \'%s\': not a valid identifier\n",str);
+    free_double(res);
+    free_double(sep);
+    return (0);
+}
+int valid_export(char *str, char **res, char **sep)
+{
+    int i;
+    int check;
+
+    check = 1;
+    i = 0;
+    if(str[i])
+    {
+        if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || str[i] == '_')
+            i++;
+        else
+            check = error_exp(str, res, sep);
+    }
+    while(str[i])
+    {   
+        if(!check)
+            return (0);
+        if(!(str[i] >= 'a' && str[i] <= 'z') || !(str[i] >= 'A' && str[i] <= 'Z') || str[i] != '_')
+            check = error_exp(str, res, sep);
+        else if(!(str[i] >= '0' && str[i] <= '9'))
+            check = error_exp(str, res, sep);
+        i++;
+    }
+    return (check);
+}
+char **append_exp(char *sep,char *holder)
+{
+    char **res;
+
+    res = (char **)malloc(sizeof(char *) * 3);
+    if(!res)
+        return (NULL);
+    res[0] = ft_strdup(sep);
+    res[1] = holder;
+    res[2] = NULL;
+    return (res);
+}
 char **separator(char *str)
 {
     int len;
@@ -29,30 +74,11 @@ char **separator(char *str)
         return (NULL);
     if(ft_strlen(str) == len)
         check = 0;
-    res = (char **)malloc(sizeof(char *) * 3);
-    if(!res)
-        return (NULL);
     holder = holder_env(str,len,check);
-    res[0] = ft_strdup(sep[0]);
-    res[1] = holder;
-    res[2] = NULL;
+    res = append_exp(sep[0],holder);
+    if(!valid_export(sep[0],res,sep))
+        return (NULL);
     free_double(sep);
     return(res);
 }
 
-void exp_print(t_path **paths)
-{
-    t_exp *tmp;
-    char *check;
-
-    tmp = (*paths)->exp_struct;
-    while(tmp)
-    {
-        check = ft_strchr(tmp->exp,'=');
-        if(check)
-            printf("declare -x %.*s=\"%s\"\n",(int)(check - tmp->exp),tmp->exp,check + 1);
-        else
-            printf("declare -x %s\n",tmp->exp);
-        tmp = tmp->next;
-    }
-}
--- builtins/export_utils_2.c
+++ builtins/export_utils_2.c
@@ -42,5 +42,22 @@ t_exp *int_exp(char **env)
     return(node_new);
 }
 
+void exp_print(t_path **paths)
+{
+    t_exp *tmp;
+    char *check;
+
+    tmp = (*paths)->exp_struct;
+    while(tmp)
+    {
+        check = ft_strchr(tmp->exp,'=');
+        if(check)
+            printf("declare -x %.*s=\"%s\"\n",(int)(check - tmp->exp),tmp->exp,check + 1);
+        else
+            printf("declare -x %s\n",tmp->exp);
+        tmp = tmp->next;
+    }
+}
+
 
 
