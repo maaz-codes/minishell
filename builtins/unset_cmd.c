4339add65ffad793da948471d5f969b48e735b37
Raphael <rcreer@lab1r5s2.42abudhabi.ae>
2024-12-04 15:13:04 +0400

    fixed the leaks on the unset cmd

--- builtins/unset_cmd.c
+++ builtins/unset_cmd.c
@@ -1,28 +1,64 @@
 #include "../minishell.h"
+
+void free_nodes(t_env *tmp_env, t_exp *tmp_exp)
+{
+    free(tmp_env->env);
+    free(tmp_env);
+    free(tmp_exp->exp);
+    free(tmp_exp);
+}
+
+void declare_struct(t_path **paths, t_env **tmp_env, t_exp **tmp_exp,int mode)
+{   
+    if(mode == 0)
+    {
+        *tmp_env = (*paths)->env_struct;
+        *tmp_exp = (*paths)->exp_struct;
+    }
+    else if(mode == 1)
+    {
+        (*paths)->env_struct = (*tmp_env)->next;
+        (*paths)->exp_struct = (*tmp_exp)->next;
+    }
+}
+
+void iterate_nodes(t_env **pr_env, t_env **tmp_env, t_exp **pr_exp, t_exp **tmp_exp)
+{   
+    *pr_env = *tmp_env;
+    *pr_exp = *tmp_exp;
+    *tmp_env = (*tmp_env)->next;
+    *tmp_exp = (*tmp_exp)->next;
+}
+
 void remove_node(t_path **paths,int pos)
 {
-    t_env *tmp;
-    t_env *prev;
+    t_env *tmp_env;
+    t_exp *tmp_exp;
+    t_env *prev_env;
+    t_exp *prev_exp;
     int i;
 
-    tmp = (*paths)->env_struct;
     i = 0;
-    while(tmp)
+    declare_struct(paths,&tmp_env,&tmp_exp,0);
+    while(tmp_env && tmp_exp)
     {
         if(i == pos)
             break;
         i++;
-        prev = tmp;
-        tmp = tmp->next;
+        iterate_nodes(&prev_env,&tmp_env,&prev_exp,&tmp_exp);
     }
-    if(tmp == NULL)
+    if(tmp_env == NULL && tmp_exp == NULL)
         return ;
-    if(prev != NULL)
-        prev->next = tmp->next;
+    if(prev_env != NULL && prev_exp != NULL)
+    {
+        prev_env->next = tmp_env->next;
+        prev_exp->next = tmp_exp->next;
+    }
     else 
-        (*paths)->env_struct = tmp->next;
-    free(tmp);
+        declare_struct(paths,&tmp_env,&tmp_exp,1);
+    free_nodes(tmp_env,tmp_exp);   
 }
+
 void unset_cmd(char **str, t_path **paths)
 {   
     t_env *tmp;
@@ -30,7 +66,6 @@ void unset_cmd(char **str, t_path **paths)
     int pos;
     
     i = 1;
-    tmp = (*paths)->env_struct;
     if(str[1] == NULL)
     {
         printf("unset: not enough arguments\n");
