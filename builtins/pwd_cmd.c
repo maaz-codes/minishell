8cbcf6a53ce07180e05347e69a33512f7c67589b
Raphael <rcreer@lab1r5s4.42abudhabi.ae>
2024-12-26 14:53:41 +0400

    error msg for cd and pwd multiple args should still work

--- builtins/pwd_cmd.c
+++ builtins/pwd_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd_cmd.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maakhan <maakhan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: rcreer <rcreer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 17:39:22 by maakhan           #+#    #+#             */
-/*   Updated: 2024/12/24 11:11:35 by maakhan          ###   ########.fr       */
+/*   Updated: 2024/12/26 14:47:46 by rcreer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,18 @@ void	pwd_cmd(char **str, t_shl *shl)
 {
 	char	cwd[1024];
 
-	if (str[1] != NULL)
+	if(!ft_strncmp(str[0],"pwd",3))
 	{
-		printf("pwd: too many arguments\n");
-		return ;
-	}
-	if (getcwd(cwd, sizeof(cwd)) != NULL)
-	{
-		printf("%s\n", cwd);
-		g_signal_caught = 0;
-		shl->exit_status = 0;
-	}
-	else
-	{
-		printf("error");
-		nuke(shl, TNT);
-		exit(EXIT_FAILURE);
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+		{
+			printf("%s\n", cwd);
+			g_signal_caught = 0;
+			shl->exit_status = 0;
+		}
+		else
+		{
+			printf(".\n");
+			return ;
+		}
 	}
 }
